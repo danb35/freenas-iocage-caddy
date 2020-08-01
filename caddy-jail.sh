42240062da5a6d95a5d03ce13aad52deff67b44f
Basil Hendroff <63370329+basilhendroff@users.noreply.github.com>
2020-08-02 00:44:21 +0800

    Do not overwrite Caddyfile if it already exists
    cp -n (copy noclobber) displays a failed copy message if a Caddyfile already exists
    The new logic avoids any error message. It only copies a sample Caddyfile if A Caddyfile doesn't already exist.

--- caddy-jail.sh
+++ caddy-jail.sh
@@ -148,7 +148,7 @@ fi
 
 # Copy pre-written config files
 iocage exec "${JAIL_NAME}" cp -f /mnt/includes/caddy /usr/local/etc/rc.d/
-iocage exec "${JAIL_NAME}" cp -n /mnt/includes/Caddyfile /usr/local/www/Caddyfile
+iocage exec "${JAIL_NAME}" [ ! -f "/mnt/tank/caddy/Caddyfile" ] && cp -f freenas-iocage-caddy/includes/Caddyfile /mnt/tank/caddy/Caddyfile
 
 iocage exec "${JAIL_NAME}" sysrc caddy_enable="YES"
 iocage exec "${JAIL_NAME}" sysrc caddy_config="/usr/local/www/Caddyfile"
